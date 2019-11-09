0277108167cef8be0a0ebe1fa23b063d737af921
Ahmed omar <ahmedomarpro@Gmail.com>
2019-11-09 15:41:28 +0200

    Life Cycle navigation Fragment

--- app/src/main/java/com/ao/lifecyclejetpack/navigation/OneFragment.java
+++ app/src/main/java/com/ao/lifecyclejetpack/navigation/OneFragment.java
@@ -18,16 +18,16 @@ import com.ao.lifecyclejetpack.R;
 public class OneFragment extends Fragment {
 
 
-	public OneFragment() {
-		// Required empty public constructor
-	}
+		public OneFragment() {
+			// Required empty public constructor
+		}
 
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_one, container, false);
+			return inflater.inflate(R.layout.fragment_one, container, false);
 	}
 
 }
